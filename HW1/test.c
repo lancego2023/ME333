063fbc109ad227ec02cfff718ac299b4f67d7138
lancego2023 <lancego2023@u.northwestern.edu>
2022-01-10 16:59:56 -0600

    finished assignment

---
+++ HW1/test.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    unsigned char i = 60, j = 80, k = 200, sum;
+
+    sum = j + k;
+
+
+    
+
+    printf("%d \n", sum);
+
+    return 0;
+
+}
+
+
+/*
+
+    (a) f = a/b = 0
+    (b) f = ((float) a)/b = 0.666667
+    (c) f = (float) (a/b) = 0;
+    (d) c = e/d = 3;
+    (e) c = (int) (e/d) = 3;
+    (f) f = ((int) e)/d = 3;
+
+    */
